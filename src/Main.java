7e78439fd82a6347144d7569c3015c8e03c915e2
pawelb92 <pawel.brzezinski92@gmail.com>
2022-10-26 20:39:40 +0200

    Królik change length wyświetlanie

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Rabbit rabbit1 = new Rabbit(50 ,"Gina",true);
         rabbit1.changeName("Super");
+        rabbit1.viewName();
+        System.out.println(rabbit1.viewName());
+        rabbit1.changeSoftness(false);
+        rabbit1.viewSoftness();
+        System.out.println(rabbit1.viewSoftness());
+        rabbit1.changeLength(25);
+        rabbit1.viewLength();
+        System.out.println(rabbit1.viewLength());
     }
 }
--- src/Rabbit.java
+++ src/Rabbit.java
@@ -10,7 +10,20 @@ public class  Rabbit {
     }
     public void changeName(String name){
          this.name = name;
-
     }
-
+    public String viewName(){
+         return name;
+    }
+    public void changeSoftness(boolean softness){
+         this.softness = softness;
+    }
+    public boolean viewSoftness(){
+         return softness;
+    }
+    public void changeLength(int length){
+         this.length = length;
+    }
+    public int viewLength(){
+         return length;
+    }
 }
