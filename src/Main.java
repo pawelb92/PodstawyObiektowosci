01fb056b9e3c70648c5eaaa8444639d7276eee5b
pawelb92 <pawel.brzezinski92@gmail.com>
2022-10-26 19:30:08 +0200

    Królik

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Rabbit rabbit1 = new Rabbit(50 ,"Gina",true);
+    }
+}
---
+++ src/Rabbit.java
@@ -0,0 +1,12 @@
+public class Rabbit {
+    int length;
+    String name;
+    boolean softness;
+
+    Rabbit(int length, String name,boolean softness){
+        this.length = length;
+        this.name = name;
+        this.softness = softness;
+    }
+
+}
